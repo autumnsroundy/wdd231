939e7aa5bf559403a12ab2cbfd6ceff4e67dc007
autumnsroundy <rou24001@byui.edu>
2025-01-25 19:44:24 -0500

    Inital NPS03 to Github

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,27 @@
-import { getParkData, parkInfoLinks } from "./parkService.mjs";
-import {
-  parkInfoTemplate,
-  mediaCardTemplate,
-  footerTemplate
-} from "./templates.mjs";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
-const parkData = getParkData();
 
-//step 1 on todo list
 function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
-  introEl.innerHTML = `<h1>${parkData.fullName}</h1><p>${parkData.description}</p>`;
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
 }
 
-//step 3 on todo list:
 function setParkInfoLinks(data) {
   const infoEl = document.querySelector(".info");
-  //Loop through and run mediaCardTemplate -take HTML returned and insert into info section (MAP)
-  const html = data.map(mediaCardTemplate); //step 4 on todo list
-  infoEl.innerHTML = html.join("");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
+  const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-setHeaderInfo(parkData);
-setHeaderFooter(parkData);
-setParkIntro(parkData);
-setParkInfoLinks(parkInfoLinks);
-setFooter(parkData);
+async function init() {
+  const parkData = await getParkData();
+  const links = getInfoLinks(parkData.images);
+  setHeaderFooter(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
+}
+
+init();
